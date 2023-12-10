8bbd0e0a69e0beb17f413a6faae4ec3f58790751
rishi-vakharia <rishi.rvv@gmail.com>
2023-12-10 13:41:44 +0530

    morgan logs (from jasvin)

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import fs from 'fs';
 
 //components
 import Connection from './database/db.js';
 import Router from './routes/route.js';
 
 import logger from './utils/logger.js'
+
 
 dotenv.config();
 
@@ -24,6 +27,30 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 const url = process.env.DB_URL;
+
+// ==================================
+// MORGAN LOGGING
+
+// Extract request's body, convert it to string and this output will be tagged as 
+// "data" parameter for morgan log pattern
+morgan.token('data', request => JSON.stringify(request.body))
+
+/*
+  - Add the Morgan logger with the following format specified
+  - The format is what will be printed by the server each time a request is received
+  - Don't use logging when in testing environment which is why NODE_ENV is checked
+	- In the production environment we want the logs to be written to a file
+	- fs.createWriteStream() writes the specified logs into the access.log file in the current directory
+	- The 'a' flag is for appending
+	- In the development environment we want the logs to be printed to the console
+	- And the second app.use(morgan()) call does exactly that
+*/
+
+app.use(morgan(':date[web] :method :url :status :res[content-length] - :response-time ms :data', {
+    stream: fs.createWriteStream('./logs/access.log', {flags: 'a'})
+}))
+
+// ==================================
 
 // Connection(username, password);
 Connection(url);
